c27691ef9d22a67bbb62c73ef939b710628b7fd2
Jeevan Chand <jibuchand11@gmail.com>
2024-09-14 22:14:57 +0530

    added branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://JeevanCd.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'JeevanCd', // Usually your GitHub org/user name.
+  projectName: 'JeevanCd.github.io', // Usually your repo name.
+  deploymentBranch: 'deploy',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
